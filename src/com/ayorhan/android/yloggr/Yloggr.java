bb37db0da5c5955ea5194b781f54b3bdb1473395
Arda Yigithan Orhan <ayorhan@ayorhan.com>
2013-05-30 17:50:18 -0700

    Got rid off some hard come and now yLoggr supports more data structure

--- src/com/ayorhan/android/yloggr/Yloggr.java
+++ src/com/ayorhan/android/yloggr/Yloggr.java
@@ -2,24 +2,17 @@ package com.ayorhan.android.yloggr;
 
 import android.util.Log;
 
-import java.security.PublicKey;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.*;
 
 /**
  * Created with IntelliJ IDEA.
  * User: ayorhan
  * Date: 10/20/12
  * Time: 2:00 PM
- * To change this template use File | Settings | File Templates.
  */
-// TODO log levels -> Package - Class - Method -> Tags
-// first look at in app log level, then system
-
-//TODO different data types
-
 public class YLoggr {
 
     public static final int VERBOSE = 2;
@@ -30,133 +23,242 @@ public class YLoggr {
     public static final int ASSERT = 7;
 
     private static final int DEFAULT_LEVEL = Log.INFO;
+    private static final String INFO_STACK_STRING = "logObject";
+    private static final String TAG_STACK_STRING = "android.app.Activity";
+    private static final String YLOGGR = "yLoggr";
+    private static final String UNKNOWN = "Unknown";
     private static int logLevel = DEFAULT_LEVEL;
 
-    static class LogLevelComparator implements Comparator<Integer> {
-        @Override
-        public int compare(Integer integer, Integer integer2) {
-            return integer - integer2;
-        }
-    }
-
-    private static HashMap<String, Integer> tagMap = new HashMap<String, Integer>();
-
-
-    //========= DEBUG ==========
-    public static void d(String message){
-        if (isLoggable("", DEBUG)){
-            Log.d(getTag(), message);
-        }
-    }
-
-    public static void d(String message, Throwable throwable){
-        if (isLoggable("", DEBUG)){
-            Log.d(getTag(), message, throwable);
-        }
-    }
-
-    public static void d(String message, Object object){
-        d(message + ":" + logObject(object));
-    }
-
+    /**
+     *
+     * @param object Object desired to be logged.
+     */
     public static void d(Object object){
-        d(logObject(object));
+        if (isLoggable("", DEBUG)){
+            Log.d(getTag(), logObject(object));
+        }
     }
 
-    // ========= INFO ========
-    public static void i(String message){
+    /**
+     *
+     * @param object Object desired to be logged.
+     * @param throwable Throwable desired to be logged.
+     */
+    public static void d(Object object, Throwable throwable){
+        if (isLoggable("", DEBUG)){
+            Log.d(getTag(), logObject(object), throwable);
+        }
+    }
+
+    /**
+     *
+     * @param object Object desired to be logged.
+     */
+    public static void i(Object object){
         if (isLoggable("", INFO)){
-            Log.i(getTag(), message);
+            Log.i(getTag(), logObject(object));
         }
     }
 
-    public static void i(String message, Throwable throwable){
+    /**
+     *
+     * @param object Object desired to be logged.
+     * @param throwable Throwable desired to be logged.
+     */
+    public static void i(Object object, Throwable throwable){
         if (isLoggable("", INFO)){
-            Log.i(getTag(), message, throwable);
+            Log.i(getTag(), logObject(object), throwable);
         }
     }
 
-    // ======== ERROR =========
-    public static void e(String message){
+    /**
+     *
+     * @param object Object desired to be logged.
+     */
+    public static void e(Object object){
         if (isLoggable("", ERROR)){
-            Log.e(getTag(), message);
+            Log.e(getTag(), logObject(object));
         }
     }
 
-    public static void e(String message, Throwable throwable){
+    /**
+     *
+     * @param object Object desired to be logged.
+     * @param throwable Throwable desired to be logged.
+     */
+    public static void e(Object object, Throwable throwable){
         if (isLoggable("", ERROR)){
-            Log.e(getTag(), message, throwable);
+            Log.e(getTag(), logObject(object), throwable);
         }
     }
 
-    // ======== VERBOSE =======
-    public static void v(String message){
+    /**
+     *
+     * @param object Object desired to be logged.
+     */
+    public static void v(Object object){
         if (isLoggable("", VERBOSE)){
-            Log.v(getTag(), message);
+            Log.v(getTag(), logObject(object));
         }
     }
 
-    public static void v(String message, Throwable throwable){
+    /**
+     *
+     * @param object Object desired to be logged.
+     * @param throwable Throwable desired to be logged.
+     */
+    public static void v(Object object, Throwable throwable){
         if (isLoggable("", VERBOSE)){
-            Log.v(getTag(), message, throwable);
+            Log.v(getTag(), logObject(object), throwable);
         }
     }
 
-    // ======== WARN ===========
-    public static void w(String message){
+    /**
+     *
+     * @param object Object desired to be logged.
+     */
+    public static void w(Object object){
         if (isLoggable("", WARN)){
-            Log.w(getTag(), message);
+            Log.w(getTag(), logObject(object));
         }
     }
 
-    public static void w(String message, Throwable throwable){
+    /**
+     *
+     * @param object Object desired to be logged.
+     * @param throwable Throwable desired to be logged.
+     */
+    public static void w(Object object, Throwable throwable){
         if (isLoggable("", WARN)){
-            Log.w(getTag(), message, throwable);
+            Log.w(getTag(), logObject(object), throwable);
         }
     }
 
-    // ======= PRIVATE ======
-    private static String getTag(){ //TODO dont hardcode
-        String fullClassName = Thread.currentThread().getStackTrace()[4].getClassName();
-        String classNameShort = fullClassName.substring(fullClassName.lastIndexOf(".") + 1);
-        String methodName = Thread.currentThread().getStackTrace()[4].getMethodName();
-        int lineNumber = Thread.currentThread().getStackTrace()[4].getLineNumber();
-
-
-        return "YLoggr" + ":" + classNameShort + "." + methodName + "()[line" + lineNumber+"]";
+    /**
+     *
+     * @return tag
+     */
+    private static String getTag(){
+        StackTraceElement[] trace = Thread.currentThread().getStackTrace();
+        int stackIndex = getStackIndex(trace, false);
+        if (stackIndex != -1){
+            String fullClassName = Thread.currentThread().getStackTrace()[stackIndex].getClassName();
+            String classNameShort = fullClassName.substring(fullClassName.lastIndexOf(".") + 1);
+            return classNameShort;
+        }
+        return YLOGGR;
     }
 
+    /**
+     *
+     * @param object Object desired to be logged.
+     * @return info
+     */
+    private static String getInfo(Object object){
+        StackTraceElement[] trace = Thread.currentThread().getStackTrace();
+        String info = "%s(){%s}[line%d] ";
+        int stackIndex = getStackIndex(trace, true);
+        if (stackIndex != -1){
+            String methodName = Thread.currentThread().getStackTrace()[stackIndex].getMethodName();
+            int lineNumber = Thread.currentThread().getStackTrace()[stackIndex].getLineNumber();
+            return String.format(info, methodName, object.getClass().getSimpleName(), lineNumber);
+        }
+        return String.format(info, UNKNOWN, UNKNOWN, -1);
+    }
+
+    /**
+     *
+     * @param trace trace
+     * @param info flag that shows info or tag
+     * @return stackIndex
+     */
+    private static int getStackIndex(StackTraceElement[] trace, boolean info) {
+        int stackIndex = -1;
+        for (int i=0; i<trace.length; i++){
+            if (info)
+                if(trace[i].getMethodName().equals(INFO_STACK_STRING)){
+                    stackIndex = i + 2;
+                    break;
+                }
+            else
+                if(trace[i].getClassName().equals(TAG_STACK_STRING)){
+                    stackIndex = i - 1;
+                    break;
+                }
+        }
+        return stackIndex;
+    }
+
+    /*
+        Checks to see whether or not a log for the specified tag is loggable at the specified level.
+        The default level of any tag is set to INFO. This means that any level above and including INFO
+        will be logged. Before you make any calls to a logging method you should check to see if your
+        tag should be logged. You can change the default level by setting a system property:
+        'setprop log.tag.<YOUR_LOG_TAG> <LEVEL>' Where level is either
+        VERBOSE, DEBUG, INFO, WARN, ERROR, ASSERT, or SUPPRESS. SUPPRESS will turn off all logging for
+        your tag. You can also create a local.prop file that with the following in it:
+        'log.tag.<YOUR_LOG_TAG>=<LEVEL>' and place that in /data/local.prop.
+     */
+
+    /**
+     *
+     * @param tag Tag
+     * @param logLevel LogLevel
+     * @return boolean
+     */
     private static boolean isLoggable(String tag, Integer logLevel){
         return true;
         //isTagLoggable(tag, logLevel);
     }
 
-    private static Integer getInAppTagLogLevel(String tag){
-        return tagMap.containsKey(tag)? tagMap.get(tag) : -1;
-    }
-
-    private void setInAppTagLogLevel(String tag, Integer logLevel){
-        tagMap.put(tag, logLevel);
-    }
-
-    private static boolean isTagLoggable(String tag, Integer logLevel){
-        Integer level = getInAppTagLogLevel(tag);
-
-        if (level.equals(-1))
-            return Log.isLoggable(tag,logLevel);
-
-        LogLevelComparator comparator = new LogLevelComparator();
-        return (comparator.compare(level,  DEFAULT_LEVEL) == 1);
-    }
-
     //TODO recursive call for custom objects
+
+    /**
+     * Object log generator
+     * @param object Object desired to be logged.
+     * @return Object log string
+     */
     private static String logObject(Object object) {
         StringBuilder output = new StringBuilder();
-
-        if (object instanceof boolean[]){
+        output.append(getInfo(object));
+        if (object instanceof Integer){
+            output.append(String.format("%d", (Integer) object));
+            return output.toString();
+        } else if (object instanceof Boolean){
+            output.append(String.format("%b", (Boolean) object));
+            return output.toString();
+        } else if (object instanceof Double){
+            output.append(String.format("%f", (Double) object));
+            return output.toString();
+        } else if (object instanceof Float){
+            output.append(String.format("%f", (Float) object));
+            return output.toString();
+        } else if (object instanceof Byte){
+            output.append(String.format("%h", (Byte) object));
+            return output.toString();
+        } else if (object instanceof String){
+            output.append(String.format("%s", (String) object));
+            return output.toString();
+        } else if (object instanceof Character){
+            output.append(String.format("%c", (Character) object));
+            return output.toString();
+        } else if (object instanceof Long){
+            output.append(String.format("%d", ((Long) object)));
+            return output.toString();
+        } else if (object instanceof Short){
+            output.append(String.format("%d", ((Short) object)));
+            return output.toString();
+        } else if (object instanceof boolean[]){
             boolean [] temp = (boolean[]) object;
             output.append("[");
             for (boolean elem : temp)
+                output.append(elem).append(",");
+            output.setCharAt(output.length() - 1, ']');
+            return output.toString();
+        } else if (object instanceof Boolean[]){
+            Boolean [] temp = (Boolean[]) object;
+            output.append("[");
+            for (Boolean elem : temp)
                 output.append(elem).append(",");
             output.setCharAt(output.length() - 1, ']');
             return output.toString();
@@ -167,10 +269,24 @@ public class YLoggr {
                 output.append(elem).append(",");
             output.setCharAt(output.length()-1, ']');
             return output.toString();
+        } else if (object instanceof Byte[]){
+            Byte[] temp = (Byte[]) object;
+            output.append("[");
+            for (Byte elem : temp)
+                output.append(elem).append(",");
+            output.setCharAt(output.length()-1, ']');
+            return output.toString();
         } else if (object instanceof short[]){
             short [] temp = (short[]) object;
             output.append("[");
             for (short elem : temp)
+                output.append(elem).append(",");
+            output.setCharAt(output.length()-1, ']');
+            return output.toString();
+        } else if (object instanceof Short[]){
+            Short [] temp = (Short[]) object;
+            output.append("[");
+            for (Short elem : temp)
                 output.append(elem).append(",");
             output.setCharAt(output.length()-1, ']');
             return output.toString();
@@ -181,10 +297,24 @@ public class YLoggr {
                 output.append("\'").append(elem).append("\'").append(",");
             output.setCharAt(output.length()-1, ']');
             return output.toString();
+        } else if (object instanceof Character[]){
+            Character [] temp = (Character[]) object;
+            output.append("[");
+            for (Character elem : temp)
+                output.append("\'").append(elem).append("\'").append(",");
+            output.setCharAt(output.length()-1, ']');
+            return output.toString();
         } else if (object instanceof int[]){
             int [] temp = (int[]) object;
             output.append("[");
             for (int elem : temp)
+                output.append(elem).append(",");
+            output.setCharAt(output.length()-1, ']');
+            return output.toString();
+        } else if (object instanceof Integer[]){
+            Integer [] temp = (Integer[]) object;
+            output.append("[");
+            for (Integer elem : temp)
                 output.append(elem).append(",");
             output.setCharAt(output.length()-1, ']');
             return output.toString();
@@ -195,10 +325,24 @@ public class YLoggr {
                 output.append(elem).append(",");
             output.setCharAt(output.length()-1, ']');
             return output.toString();
+        } else if (object instanceof Long[]){
+            Long [] temp = (Long[]) object;
+            output.append("[");
+            for (Long elem : temp)
+                output.append(elem).append(",");
+            output.setCharAt(output.length()-1, ']');
+            return output.toString();
         } else if (object instanceof float[]){
             float [] temp = (float[]) object;
             output.append("[");
             for (float elem : temp)
+                output.append(elem).append(",");
+            output.setCharAt(output.length()-1, ']');
+            return output.toString();
+        } else if (object instanceof Float[]){
+            Float [] temp = (Float[]) object;
+            output.append("[");
+            for (Float elem : temp)
                 output.append(elem).append(",");
             output.setCharAt(output.length()-1, ']');
             return output.toString();
@@ -209,7 +353,14 @@ public class YLoggr {
                 output.append(elem).append(",");
             output.setCharAt(output.length()-1, ']');
             return output.toString();
-        }else if (object instanceof String[]){
+        } else if (object instanceof Double[]){
+            Double [] temp = (Double[]) object;
+            output.append("[");
+            for (Double elem : temp)
+                output.append(elem).append(",");
+            output.setCharAt(output.length()-1, ']');
+            return output.toString();
+        } else if (object instanceof String[]){
             String [] temp = (String[]) object;
             output.append("[");
             for (String elem : temp)
@@ -222,13 +373,40 @@ public class YLoggr {
                 o.toString();
             }
             return null;
+        } else { //Custom
+            for (Field f : object.getClass().getFields()){
+                Class fieldType = f.getType();
+                String fieldName = f.getName();
 
-        } else if (object instanceof Integer){
-             return null;
-        } else { //Custom Object
-            throw new IllegalArgumentException("This type is not supported by YLoggr.");
+                if(fieldType.isArray()){
+                    Class componentType = fieldType.getComponentType();
+
+                    if (componentType == Boolean.class ||
+                            componentType == Boolean.TYPE ||
+                            componentType == Integer.class ||
+                            componentType == Integer.TYPE ||
+                            componentType == Long.class ||
+                            componentType == Long.TYPE ||
+                            componentType == Float.class ||
+                            componentType == Float.TYPE ||
+                            componentType == Double.class ||
+                            componentType == Double.TYPE ||
+                            componentType == String.class) {
+
+
+                    } else {
+
+                    }
+
+                } else if (Collection.class.isAssignableFrom(fieldType)){
+                    Type genericType = f.getGenericType();
+                    Class componentType = (Class) ((ParameterizedType) genericType)
+                            .getActualTypeArguments()[0];
+                } else{
+
+                }
+            }
+            return "This type is not supported by YLoggr.";
         }
-
-
     }
 }
