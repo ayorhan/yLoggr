1eeb91b849972e3cf59592e47e14071039c68350
Arda Yigithan Orhan <ayorhan@ayorhan.com>
2013-06-01 21:26:05 -0700

    Added missing methods

--- src/com/ayorhan/android/yloggr/Yloggr.java
+++ src/com/ayorhan/android/yloggr/Yloggr.java
@@ -12,6 +12,33 @@ import java.util.*;
  * User: ayorhan
  * Date: 10/20/12
  * Time: 2:00 PM
+ *
+ * API for sending log output.
+ *
+ * Generally, use the yLoggr.v() yLoggr.d() yLoggr.i() yLoggr.w() and yLoggr.e() methods.
+ *
+ * The order in terms of verbosity, from least to most is ERROR, WARN, INFO, DEBUG, VERBOSE.
+ * Verbose should never be compiled into an application except during development.
+ * Debug logs are compiled in but stripped at runtime. Error, warning and info logs are always kept.
+ *
+ * Tip: You don't need to declare a TAG, yLoggr will automatically grab the class name as the TAG.
+ * For example, if the class name is MyActivity.java, TAG will be MyActivity.
+ *
+ * Tip: Don't forget that when you make a call like
+ *
+ *      yLoggr.v("index=" + i);
+ *
+ * that when you're building the string to pass into yLoggr.d, the compiler uses a StringBuilder and
+ * at least three allocations occur: the StringBuilder itself, the buffer, and the String object.
+ *
+ * Realistically, there is also another buffer allocation and copy, and even more pressure on the gc.
+ * That means that if your log message is filtered out, you might be doing significant work and
+ * incurring significant overhead.
+ */
+
+/*
+    TODO #1 In app general log level - if set
+    TODO #2 In app TAG log level
  */
 public class YLoggr {
 
@@ -23,120 +50,192 @@ public class YLoggr {
     public static final int ASSERT = 7;
 
     private static final int DEFAULT_LEVEL = Log.INFO;
+
     private static final String INFO_STACK_STRING = "logObject";
     private static final String TAG_STACK_STRING = "android.app.Activity";
     private static final String YLOGGR = "yLoggr";
     private static final String UNKNOWN = "Unknown";
-    private static int logLevel = DEFAULT_LEVEL;
 
     /**
-     *
-     * @param object Object desired to be logged.
+     * Send a DEBUG log message.
+     * @param object The object you would like logged.
      */
     public static void d(Object object){
-        if (isLoggable("", DEBUG)){
+        if (isLoggable(getTag(), DEBUG)){
             Log.d(getTag(), logObject(object));
         }
     }
 
     /**
-     *
-     * @param object Object desired to be logged.
-     * @param throwable Throwable desired to be logged.
+     * Send a DEBUG log message and log the exception.
+     * @param object The object you would like logged.
+     * @param tr An exception to log
      */
-    public static void d(Object object, Throwable throwable){
-        if (isLoggable("", DEBUG)){
-            Log.d(getTag(), logObject(object), throwable);
+    public static void d(Object object, Throwable tr){
+        if (isLoggable(getTag(), DEBUG)){
+            Log.d(getTag(), logObject(object), tr);
         }
     }
 
     /**
-     *
-     * @param object Object desired to be logged.
-     */
-    public static void i(Object object){
-        if (isLoggable("", INFO)){
-            Log.i(getTag(), logObject(object));
-        }
-    }
-
-    /**
-     *
-     * @param object Object desired to be logged.
-     * @param throwable Throwable desired to be logged.
-     */
-    public static void i(Object object, Throwable throwable){
-        if (isLoggable("", INFO)){
-            Log.i(getTag(), logObject(object), throwable);
-        }
-    }
-
-    /**
-     *
-     * @param object Object desired to be logged.
+     * Send an ERROR log message.
+     * @param object The object you would like logged.
      */
     public static void e(Object object){
-        if (isLoggable("", ERROR)){
+        if (isLoggable(getTag(), ERROR)){
             Log.e(getTag(), logObject(object));
         }
     }
 
     /**
-     *
-     * @param object Object desired to be logged.
-     * @param throwable Throwable desired to be logged.
+     * Send an ERROR log message and log the exception.
+     * @param object The object you would like logged.
+     * @param tr An exception to log
      */
-    public static void e(Object object, Throwable throwable){
-        if (isLoggable("", ERROR)){
-            Log.e(getTag(), logObject(object), throwable);
+    public static void e(Object object, Throwable tr){
+        if (isLoggable(getTag(), ERROR)){
+            Log.e(getTag(), logObject(object), tr);
         }
     }
 
     /**
-     *
-     * @param object Object desired to be logged.
+     * Handy function to get a loggable stack trace from a Throwable
+     * @param tr An exception to log
+     * @return
+     */
+    public static String getStackTraceString(Throwable tr){
+        return Log.getStackTraceString(tr);
+    }
+
+    /**
+     * Send an INFO log message.
+     * @param object The object you would like logged.
+     */
+    public static void i(Object object){
+        if (isLoggable(getTag(), INFO)){
+            Log.i(getTag(), logObject(object));
+        }
+    }
+
+    /**
+     * Send an INFO log message and log the exception.
+     * @param object The object you would like logged.
+     * @param tr An exception to log
+     */
+    public static void i(Object object, Throwable tr){
+        if (isLoggable(getTag(), INFO)){
+            Log.i(getTag(), logObject(object), tr);
+        }
+    }
+
+       /*
+        Checks to see whether or not a log for the specified tag is loggable at the specified level.
+        The default level of any tag is set to INFO. This means that any level above and including INFO
+        will be logged. Before you make any calls to a logging method you should check to see if your
+        tag should be logged. You can change the default level by setting a system property:
+        'setprop log.tag.<YOUR_LOG_TAG> <LEVEL>' Where level is either
+        VERBOSE, DEBUG, INFO, WARN, ERROR, ASSERT, or SUPPRESS. SUPPRESS will turn off all logging for
+        your tag. You can also create a local.prop file that with the following in it:
+        'log.tag.<YOUR_LOG_TAG>=<LEVEL>' and place that in /data/local.prop.
+     */
+
+    /**
+     * Checks to see whether or not a log for the specified tag is loggable at the specified level.
+     * @param tag The tag to check.
+     * @param logLevel The level to check.
+     * @return Whether or not that this is allowed to be logged.
+     * @throws IllegalArgumentException is thrown if the tag.length() > 23.
+     */
+    public static boolean isLoggable(String tag, Integer logLevel){
+        return Log.isLoggable(tag, logLevel);
+    }
+
+    /**
+     * Low-level logging call.
+     * @param priority The priority/type of this log message
+     * @param object The message you would like logged.
+     * @return The number of bytes written.
+     */
+    public static int println(int priority, Object object){
+        return Log.println(priority, getTag(), logObject(object));
+    }
+
+    /**
+     * Send a VERBOSE log message.
+     * @param object The object you would like logged.
      */
     public static void v(Object object){
-        if (isLoggable("", VERBOSE)){
+        if (isLoggable(getTag(), VERBOSE)){
             Log.v(getTag(), logObject(object));
         }
     }
 
     /**
-     *
-     * @param object Object desired to be logged.
-     * @param throwable Throwable desired to be logged.
+     * Send a VERBOSE log message and log the exception.
+     * @param object The object you would like logged.
+     * @param tr An exception to log
      */
-    public static void v(Object object, Throwable throwable){
-        if (isLoggable("", VERBOSE)){
-            Log.v(getTag(), logObject(object), throwable);
+    public static void v(Object object, Throwable tr){
+        if (isLoggable(getTag(), VERBOSE)){
+            Log.v(getTag(), logObject(object), tr);
         }
     }
 
     /**
-     *
-     * @param object Object desired to be logged.
+     * Log a WARN exception
+     * @param tr An exception to log
+     */
+    public static void w(Throwable tr){
+        if (isLoggable(getTag(), WARN)){
+            Log.w(getTag(), tr);
+        }
+    }
+
+    /**
+     * Send a WARN log message.
+     * @param object The object you would like logged.
      */
     public static void w(Object object){
-        if (isLoggable("", WARN)){
+        if (isLoggable(getTag(), WARN)){
             Log.w(getTag(), logObject(object));
         }
     }
 
     /**
-     *
-     * @param object Object desired to be logged.
-     * @param throwable Throwable desired to be logged.
+     * Send a WARN log message and log the exception.
+     * @param object The object you would like logged.
+     * @param tr An exception to log
      */
-    public static void w(Object object, Throwable throwable){
-        if (isLoggable("", WARN)){
-            Log.w(getTag(), logObject(object), throwable);
+    public static void w(Object object, Throwable tr){
+        if (isLoggable(getTag(), WARN)){
+            Log.w(getTag(), logObject(object), tr);
         }
     }
 
     /**
-     *
-     * @return tag
+     * Send a WARN log message.
+     * @param object The object you would like logged.
+     */
+    public static void wtf(Object object){
+        if (isLoggable(getTag(), ASSERT)){
+            Log.wtf(getTag(), logObject(object));
+        }
+    }
+
+    /**
+     * Send a WARN log message and log the exception.
+     * @param object The object you would like logged.
+     * @param tr An exception to log
+     */
+    public static void wtf(Object object, Throwable tr){
+        if (isLoggable(getTag(), ASSERT)){
+            Log.wtf(getTag(), logObject(object), tr);
+        }
+    }
+
+    /**
+     * Generate log tag automatically.
+     * @return tag Used to identify the source of a log message. It usually identifies the class or activity where the log call occurs.
      */
     private static String getTag(){
         StackTraceElement[] trace = Thread.currentThread().getStackTrace();
@@ -150,7 +249,7 @@ public class YLoggr {
     }
 
     /**
-     *
+     * Generate log info automatically.
      * @param object Object desired to be logged.
      * @return info
      */
@@ -167,7 +266,7 @@ public class YLoggr {
     }
 
     /**
-     *
+     * Get stack index
      * @param trace trace
      * @param info flag that shows info or tag
      * @return stackIndex
@@ -189,27 +288,7 @@ public class YLoggr {
         return stackIndex;
     }
 
-    /*
-        Checks to see whether or not a log for the specified tag is loggable at the specified level.
-        The default level of any tag is set to INFO. This means that any level above and including INFO
-        will be logged. Before you make any calls to a logging method you should check to see if your
-        tag should be logged. You can change the default level by setting a system property:
-        'setprop log.tag.<YOUR_LOG_TAG> <LEVEL>' Where level is either
-        VERBOSE, DEBUG, INFO, WARN, ERROR, ASSERT, or SUPPRESS. SUPPRESS will turn off all logging for
-        your tag. You can also create a local.prop file that with the following in it:
-        'log.tag.<YOUR_LOG_TAG>=<LEVEL>' and place that in /data/local.prop.
-     */
 
-    /**
-     *
-     * @param tag Tag
-     * @param logLevel LogLevel
-     * @return boolean
-     */
-    private static boolean isLoggable(String tag, Integer logLevel){
-        return true;
-        //isTagLoggable(tag, logLevel);
-    }
 
     //TODO recursive call for custom objects
 
